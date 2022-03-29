7e88bc642af101668a5a1f3066d4720d689cdb14
Souhail Benlhachemi <souhailbenlhachemi1999@gmail.com>
2022-03-29 18:02:34 +0000

    v1

---
+++ pages/account.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Account = () => {
+    return(
+        <div>
+            <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+            <center>AbuSalem775@gmail.com:HAm887!m</center>
+        </div>
+    )
+};
+
+export default Account;
